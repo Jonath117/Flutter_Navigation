fb78910bb7f40d00ad22739f87b46eb477d86f5e
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-05 13:18:48 -0400

    refactorizacion usando la logica de estados  y eventos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,50 +1,43 @@
 import 'package:flutter/material.dart';
-import '../models/task_model.dart';
+import 'package:flutter_app_navigations/blocs/task_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_app_navigations/screens/task_detail_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static String path = '/';
 
-  HomeScreen({super.key});
-
-  final List<TaskModel> tareas = [
-    TaskModel(
-      id: 1,
-      title: "Tarea 1",
-      status: "Pendiente",
-      assignee: "Jonathan",
-      description: "Hacer la tarea 1",
-    ),
-    TaskModel(
-      id: 2,
-      title: "Actualizar datos",
-      status: "en progreso",
-      assignee: "Enzo",
-      description: "Migrar base de datos",
-    ),
-    TaskModel(
-      id: 3,
-      title: "Terminar tarea Power BI",
-      status: "atrasada",
-      assignee: "Enzo",
-      description: "terminar el dashboard",
-    ),
-  ];
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home -- Mis Tareas")),
-      body: ListView.builder(
-        itemCount: tareas.length,
-        itemBuilder: (context, index) {
-          final tarea = tareas[index];
-          return ListTile(
-            title: Text(tarea.title),
-            subtitle: Text('${tarea.status} - ${tarea.assignee}'),
-            onTap: () {
-              Navigator.pushNamed(context, '/task-detail', arguments: tarea);
-            },
-          );
+      body: BlocBuilder<TaskBloc, TaskState>(
+        builder: (context, state) {
+          if (state is TaskInitial) {
+            context.read<TaskBloc>().add(LoadTasks());
+          }
+
+          if (state is TaskListLoaded) {
+            return ListView.builder(
+              itemCount: state.tasks.length,
+              itemBuilder: (context, index) {
+                final task = state.tasks[index];
+                return ListTile(
+                  title: Text(task.title),
+                  subtitle: Text('Asignado a: ${task.assignee}'),
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      TaskDetailScreen.path,
+                      arguments: task,
+                    );
+                  },
+                );
+              },
+            );
+          }
+          return Center(child: Text('Algo salio mal'));
         },
       ),
     );
